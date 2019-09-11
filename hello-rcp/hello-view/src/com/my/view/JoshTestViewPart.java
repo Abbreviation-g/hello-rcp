498139a8bd393caa13a1e64e81e3d8c723363efc
guoenjing <guoenjing@joshvm.com>
2019-09-11 15:29:52 +0800

    添加外置插件

--- hello-rcp/hello-view/src/com/my/view/JoshTestViewPart.java
+++ hello-rcp/hello-view/src/com/my/view/JoshTestViewPart.java
@@ -1,5 +1,11 @@
 package com.my.view;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,17 +21,20 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.part.ViewPart;
 
-public class JoshTestViewPart extends ViewPart implements ISelectionListener, ISelectionChangedListener{
+public class JoshTestViewPart extends ViewPart implements ISelectionListener, ISelectionChangedListener {
 
 	public JoshTestViewPart() {
 	}
 
 	private List<Button> buttons = new ArrayList<Button>();
 	private ListViewer listViewer;
+	private Text text;
+
 	public void selectionChanged(SelectionChangedEvent event) {
 		System.out.println("JoshTestViewPart.selectionChanged()");
 	}
@@ -47,7 +56,7 @@ public class JoshTestViewPart extends ViewPart implements ISelectionListener, IS
 		group.setText("Test Group");
 		group.setLayoutData(new GridData(GridData.FILL_BOTH));
 		group.setLayout(new GridLayout());
-		
+
 		Button button1 = new Button(group, SWT.PUSH);
 		button1.setText("button1");
 		button1.setLayoutData(new GridData(GridData.FILL_BOTH));
@@ -57,30 +66,48 @@ public class JoshTestViewPart extends ViewPart implements ISelectionListener, IS
 		Button button3 = new Button(group, SWT.PUSH);
 		button3.setText("button3");
 		button3.setLayoutData(new GridData(GridData.FILL_BOTH));
-		
+
 		buttons.add(button1);
 		buttons.add(button2);
 		buttons.add(button3);
-		
+
 		listViewer = new ListViewer(group);
 		listViewer.getList().setLayoutData(new GridData(GridData.FILL_BOTH));
 		listViewer.setContentProvider(new ArrayContentProvider());
 		listViewer.setLabelProvider(new LabelProvider());
-		listViewer.setInput(new String[]{"1","2","3","4"});
-		
+		listViewer.setInput(new String[] { "1", "2", "3", "4" });
+
 		getSite().setSelectionProvider(listViewer);
+
+		this.text = new Text(group, SWT.BORDER | SWT.MULTI);
+		GridData gridData = new GridData(GridData.FILL_BOTH);
+		gridData.heightHint = 300;
+		text.setLayoutData(gridData);
 	}
 
 	@Override
 	public void setFocus() {
-		
+		try {
+			URL url = new URL("https://fanyi.baidu.com");
+			InputStream inputStream = url.openStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				text.append(line);
+			}
+			reader.close();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void selectionChanged(IWorkbenchPart part, ISelection selection) {
 		System.out.println("JoshTestViewPart.selectionChanged()");
 	}
-	
+
 //	private void pageSelectionChanged(SelectionChangedEvent event) {
 //		// forward this change from a page to our site's selection provider
 //		ListViewer provider = (ListViewer) getSite()
